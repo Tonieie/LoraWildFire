5b72e467d158fd30736add2492144d100acdb79d
Tonieie <tonzazil12@gmail.com>
2021-05-30 13:28:48 +0700

    added fire and timestamp

--- WebDashboard/public/index.js
+++ WebDashboard/public/index.js
@@ -1,4 +1,4 @@
-var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+var margin = { top: 20, right: 30, bottom: 30, left: 60 },
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
